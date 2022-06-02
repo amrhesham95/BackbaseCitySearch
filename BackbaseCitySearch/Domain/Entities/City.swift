997ed07055d3267b5607a5b41f6b52ef1babd772
amrhesham95 <47639999+amrhesham95@users.noreply.github.com>
2022-06-02 23:03:17 +0200

    add entities (City, Coordination)

---
+++ BackbaseCitySearch/Domain/Entities/City.swift
@@ -0,0 +1,20 @@
+//
+//  City.swift
+//  BackbaseCitySearch
+//
+//  Created by Amr Hesham on 02/06/2022.
+//
+
+import Foundation
+// MARK: - City
+struct City: Decodable {
+    let country, name: String?
+    let id: Int?
+    let coordination: Coordination?
+
+    enum CodingKeys: String, CodingKey {
+        case country, name
+        case id = "_id"
+        case coordination = "coord"
+    }
+}
---
+++ BackbaseCitySearch/Domain/Entities/Coordination.swift
@@ -0,0 +1,15 @@
+//
+//  Coordination.swift
+//  BackbaseCitySearch
+//
+//  Created by Amr Hesham on 02/06/2022.
+//
+
+import Foundation
+
+
+
+// MARK: - Coord
+struct Coordination: Codable {
+    let lon, lat: Double?
+}
