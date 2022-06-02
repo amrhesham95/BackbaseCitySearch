03fd2f4c2ca8016114088da85771ec0eb066b8ce
amrhesham95 <47639999+amrhesham95@users.noreply.github.com>
2022-06-02 23:33:50 +0200

    - add CitiesFetcher - add LocalCitiesFetcher - add BackbaseError - add Constants

---
+++ BackbaseCitySearch/Application/BackbaseError.swift
@@ -0,0 +1,12 @@
+//
+//  BackbaseError.swift
+//  BackbaseCitySearch
+//
+//  Created by Amr Hesham on 02/06/2022.
+//
+
+import Foundation
+
+enum BackbaseError: Error {
+    case fileReadingError
+}
---
+++ BackbaseCitySearch/Application/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  BackbaseCitySearch
+//
+//  Created by Amr Hesham on 02/06/2022.
+//
+
+import Foundation
+enum Constants {
+    static var citiesFileName = "cities"
+}
